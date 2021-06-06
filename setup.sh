041396d96f63feeebfac52d6f9b371d48d2b6b96
EdvardOlsen <83424516+EdvardOlsen@users.noreply.github.com>
2021-06-06 16:40:56 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+pip install torch==1.4.0
+pip3 install transformers==3.5.0
+pip install pretty_midi
+pip install pychord
+wget https://musical-artifacts.com/artifacts/1583/megalovania_smash_guitar.sf2
+wget https://musical-artifacts.com/artifacts/1353/Ukulele_little-scale.sf2
+sudo apt-get install fluidsynth
+pip install beautifulsoup4
+pip3 install transformers
+git clone https://github.com/huggingface/transformers.git
+cd transformers/ && git checkout v2.2.0 
+git clone https://github.com/EdvardOlsen/Horoscope_generator.git
